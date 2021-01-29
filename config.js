ef72e4c78c8e30b6e49bf048df77b0a26a5141a1
Dan Roche <52405038+danroche10@users.noreply.github.com>
2021-01-29 18:12:21 +0000

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {	
+  MONGODB:	
+    "mongodb+srv://danroche10:dr061091@cluster0.0cqdr.mongodb.net/database1?retryWrites=true&w=majority",	
+
+  SECRET_KEY: "some very secret key"	
+};
